f121b797d0987310ac82a5ed521bb2205a3cb1dc
[LoLoo03] <[bloren641@gmail.com]>
2025-04-30 14:28:53 -0300

    Creo Clase Rating

--- app/models.py
+++ app/models.py
@@ -73,3 +73,31 @@ class Event(models.Model):
         self.organizer = organizer or self.organizer
 
         self.save()
+
+class Rating(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rating_users")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="rating_events")
+    
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def validate(cls, title, text, rating ,created_at):
+        errors = {}
+
+        if title is None:
+            errors["title"] = "Por favor ingrese un titulo"
+
+        if text is None:
+            errors["text"] = "Por favor ingrese un texto"
+        
+        if rating is None:
+            errors["rating"] = "Por favor ingrese un rating"
+
+        return errors
+    
+    @classmethod
